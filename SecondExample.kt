1ea03b44c8ae7435fbc46c7dd8bfb62a15141535
Artem <artem.shumidub@gmail.com>
2019-08-14 08:47:44 +0300

    Update SecondExample.kt

--- SecondExample.kt
+++ SecondExample.kt
@@ -1,9 +1,5 @@
-fun main(args: Array&lt;String&gt;) {
-//sampleStart
 fun sum(firstNum: Int, secondNum: Int) : Int {
   return firstNum + secondNum                   
 }
     
 print(sum(2,3))
-//sampleEnd
-}
